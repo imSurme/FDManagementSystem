807cb0325b3c1e9b00c3f749296780a96dac27c4
Celil Aslan <92295880+celilaslan@users.noreply.github.com>
2024-11-10 23:43:44 +0300

    Add schema and data files
    Create Restaurants table and insert data

---
+++ database/food_delivery-schema.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE food_delivery;
+USE food_delivery;
+
+CREATE TABLE Users (
+
+);
+
+CREATE TABLE Restaurants (
+    restaurant_id INT AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    restaurant_name VARCHAR(100) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    rating DECIMAL(2,1),
+    rating_count VARCHAR(50),
+    average_cost INT CHECK(average_cost >= 0),
+    cuisine VARCHAR(100) NOT NULL,
+    restaurant_address TEXT NOT NULL,
+    PRIMARY KEY (restaurant_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Orders (
+
+);
+
+CREATE TABLE Food (
+
+);
+
+CREATE TABLE Couriers (
+
+);
+
+
+CREATE TABLE Menu (
+
+);
