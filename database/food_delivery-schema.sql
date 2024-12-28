39e7a6af7136cd888685fff53916906e83940e25
İbrahim Mert Sürme <119628481+imSurme@users.noreply.github.com>
2024-12-29 01:30:55 +0300

    Update food_delivery-schema.sql

--- database/food_delivery-schema.sql
+++ database/food_delivery-schema.sql
@@ -10,31 +10,28 @@ CREATE TABLE admins (
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    age INT NOT NULL CHECK(age >= 12),
-    gender VARCHAR(15) NOT NULL,
-    marital_status VARCHAR(30) NOT NULL,
-    occupation VARCHAR(30) NOT NULL,
-    monthly_income VARCHAR(30) NOT NULL,
-    educational_qualifications VARCHAR(30) NOT NULL,
-    family_size INT NOT NULL CHECK(family_size >= 0),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    age INT NOT NULL CHECK(age >= 18),
+    gender gender ENUM('Male','Female') NOT NULL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE restaurants (
     restaurant_id INT AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    user_id INT,
     restaurant_name VARCHAR(100) NOT NULL,
     city VARCHAR(50) NOT NULL,
     rating DECIMAL(2,1),
-    rating_count VARCHAR(50),
-    average_cost INT NOT NULL CHECK(average_cost >= 0),
+    rating_count ENUM('Too Few Ratings','20+ ratings','50+ ratings','100+ ratings','500+ ratings','1K+ ratings') NOT NULL,
+    average_cost INT NOT NULL CHECK(average_cost > 0),
     cuisine VARCHAR(100) NOT NULL,
     restaurant_address TEXT NOT NULL,
     PRIMARY KEY (restaurant_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE orders (
@@ -42,15 +39,19 @@ CREATE TABLE orders (
     order_date DATE NOT NULL,
     sales_qty FLOAT NOT NULL,
     sales_amount FLOAT NOT NULL,
-    user_id INT NOT NULL,
-    restaurant_id INT NOT NULL,
-    order_status VARCHAR(20) NOT NULL,
+    user_id INT,
+    restaurant_id INT,
+    order_status ENUM('canceled','pending','completed') NOT NULL,
     PRIMARY KEY (order_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
     FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
-CREATE TABLE food (
+CREATE TABLE foods (
     food_id INT AUTO_INCREMENT,
     item_name VARCHAR(255) NOT NULL,
     veg_or_non_veg ENUM('Veg', 'Non-veg') NOT NULL,
@@ -59,7 +60,7 @@ CREATE TABLE food (
 
 CREATE TABLE couriers (
     courier_id INT AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     gender ENUM('Male', 'Female') NOT NULL,
     birth_date DATE NOT NULL,
     restaurant_id INT,
@@ -70,13 +71,17 @@ CREATE TABLE couriers (
 );
 
 
-CREATE TABLE menu (
+CREATE TABLE menus (
     menu_id INT AUTO_INCREMENT,
     restaurant_id INT NOT NULL,
-    food_id INT NOT NULL,
+    food_id INT,
     cuisine VARCHAR(50) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL CHECK(price >= 0),
+    price DECIMAL(10, 2) NOT NULL CHECK(price > 0),
     PRIMARY KEY (menu_id),
-    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
     FOREIGN KEY (food_id) REFERENCES food(food_id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
