47173b13707f1fa734120d769102e6335a97afc9
Ata Türköz <44947835+atatrkz@users.noreply.github.com>
2024-12-30 02:46:56 +0300

    Menu page is added to the app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from index import index
 from auth import login, logout
 from restaurants import restaurants, restaurant_action
 from couriers import couriers, courier_action
+from menu import menu, menu_action
 from orders import orders, order_action
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ app.add_url_rule('/restaurants', 'restaurants', restaurants)
 app.add_url_rule('/restaurants', 'restaurant_action', restaurant_action, methods=['POST'])
 app.add_url_rule('/couriers', 'couriers', couriers)
 app.add_url_rule('/couriers', 'courier_action', courier_action, methods=['POST'])
+app.add_url_rule('/menu', 'menu', menu)
+app.add_url_rule('/menu', 'menu_action', menu_action, methods=['POST'])
 app.add_url_rule('/orders', 'orders', orders)
 app.add_url_rule('/orders', 'order_action', order_action, methods=['POST'])
 
